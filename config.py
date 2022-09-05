525ce970857b79d692012c4efb33adb65f77a26a
Your Name <volinina.e@yandex.ru>
2022-09-05 21:43:33 +0300

    Add base bot logic

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 BOT_TOKEN = "5370419822:AAFODCnjkQIi3VCeOBR0IBAVDWJ6tl2itVo"
-APP_URL="https://git.heroku.com/progress-bank-bot.git"+BOT_TOKEN
+APP_URL = "https://git.heroku.com/progress-bank-bot.git"+ BOT_TOKEN
