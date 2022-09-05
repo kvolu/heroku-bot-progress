ecb0209e5bd9eadfea1c81e3c5e1fd54674fa55d
Your Name <volinina.e@yandex.ru>
2022-09-05 22:02:30 +0300

    Add base bot logic

--- main.py
+++ main.py
@@ -14,14 +14,6 @@ logger.setLevel(logging.DEBUG)
 #googlesheet_id = 'https://docs.google.com/spreadsheets/d/1E-F6Nfm2iLEaVTN-4eh2b2uYfE10eZ_9sNDrUZsZpzY/edit#gid=0'
 #gc = gspread.service_account(filename='my-project-19798-python-bot-53bb87dd46fb.json')
 
-
-@bot.message_handler(commands=["start"])
-def start(message):
-    user_id = message.from_user.id
-    username = message.from_user.username
-    bot.reply_to(message, f"Hello, {username}!")
-
-
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
@@ -34,3 +26,10 @@ if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    user_id = message.from_user.id
+    username = message.from_user.username
+    bot.reply_to(message, f"Hello, {username}!")
