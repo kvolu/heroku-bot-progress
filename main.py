00cbcb215f55b4c6ea96ca32de72e96dc57adcaa
Your Name <volinina.e@yandex.ru>
2022-09-05 11:38:07 +0300

    add commit base bot logic

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 from config import *
 from flask import Flask, request
 
-bot = telebot.Telebot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
