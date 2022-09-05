bce99c93efd8407e5abd46cf5a682fb2b74d9c3f
Your Name <volinina.e@yandex.ru>
2022-09-05 21:32:28 +0300

    Add base bot logic

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: python3 main.py
-worker: python
+worker: python main.py
--- main.py
+++ main.py
@@ -11,12 +11,12 @@ bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
-googlesheet_id = 'https://docs.google.com/spreadsheets/d/1E-F6Nfm2iLEaVTN-4eh2b2uYfE10eZ_9sNDrUZsZpzY/edit#gid=0'
-gc = gspread.service_account(filename='my-project-19798-python-bot-53bb87dd46fb.json')
+#googlesheet_id = 'https://docs.google.com/spreadsheets/d/1E-F6Nfm2iLEaVTN-4eh2b2uYfE10eZ_9sNDrUZsZpzY/edit#gid=0'
+#gc = gspread.service_account(filename='my-project-19798-python-bot-53bb87dd46fb.json')
 
 
 ###знакомимся
-@bot.message_handler(command=['start'])
+@bot.message_handler(commands=['start'])
 def start(message):
     user_name = message.from_user.user_name
     bot.reply_to(message, f'Hello, {user_name}!')
@@ -27,7 +27,7 @@ if __name__=="__main__":
     bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
-@server.route(f"/{BOT_TOKEN}", methods = ["POST"])
+@server.route('/'+BOT_TOKEN, methods = ["POST"])
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json_string)
