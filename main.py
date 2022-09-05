2bb37d7d160c7ae78ea7cabeeeadcbb7d80f468b
Your Name <volinina.e@yandex.ru>
2022-09-05 11:51:57 +0300

    Add base bot logic

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ logger.setLevel(logging.DEBUG)
 googlesheet_id = 'https://docs.google.com/spreadsheets/d/1E-F6Nfm2iLEaVTN-4eh2b2uYfE10eZ_9sNDrUZsZpzY/edit#gid=0'
 gc = gspread.service_account(filename='my-project-19798-python-bot-53bb87dd46fb.json')
 
+
 ###знакомимся
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -25,7 +26,7 @@ if __name__=="__main__":
     bot.set_webhook(url=APP_URL)
     server.run(host = "0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
-@server.router(f"/{BOT_TOKEN}", methods = ["POST"])
+@server.route(f"/{BOT_TOKEN}", methods = ["POST"])
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json_string)
