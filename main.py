7f354c685d0bd7db5c1167516fe9b3a57a710a6c
Your Name <volinina.e@yandex.ru>
2022-09-05 12:04:28 +0300

    Add base bot logic

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ def start(message):
     user_name = message.from_user.user_name
     bot.reply_to(message, f'Hello, {user_name}!')
 
-if __name__=="__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    server.run(host = "0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
 @server.route(f"/{BOT_TOKEN}", methods = ["POST"])
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return "!", 200
+
+if __name__=="__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host = "0.0.0.0", port=int(os.environ.get("PORT", 5000)))
